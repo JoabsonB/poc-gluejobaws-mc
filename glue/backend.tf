bc52cd9aa7e2949ad2cc7937c9157b09ed260da3
JoabsonB <40070823+JoabsonB@users.noreply.github.com>
2023-10-23 13:20:37 -0300

    Update backend.tf

--- glue/backend.tf
+++ glue/backend.tf
@@ -1,10 +1,4 @@
 terraform {
   backend "s3" {
-    bucket         = "mastercard-datalake-dbm-jenkins-s3"
-    key            = "my-terraform-state-key"
-    region         = "us-east-1"
-    encrypt        = true
-    workspace_key_prefix = "dataflow-mc"
-    profile = "joabson.globalmetrics"
   }
 }
